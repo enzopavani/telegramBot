987a45af09939b3124004f715a89655662153d29
enzopavani <tijoloenso@gmail.com>
2025-05-19 14:32:01 -0300

    Goodbye command

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+import telebot
+
+bot = telebot.TeleBot("8132197812:AAEftaW-q09-Sioe3gSf_nXgcWrInxvP3cE")
+
+@bot.message_handler(['start', 'help'])
+def start(msg:telebot.types.Message):
+    bot.reply_to(msg, "Hello, World!")
+
+@bot.message_handler(['goodbye'])
+def goodbye(msg:telebot.types.Message):
+    bot.reply_to(msg, "Goodbye, World!")
+
+bot.infinity_polling()
